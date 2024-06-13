9d477da0fe9f03398630d9f54d2ae1f9d40916d3
李海天 <lhtjyjy@163.com>
2024-06-13 11:09:11 +0800

    bendi2

--- src/tt.java
+++ src/tt.java
@@ -5,7 +5,7 @@ public class tt {
     public static void main(String[] args) {
 
         System.out.println("1111111");
-        System.out.println("1111111");
+        System.out.println("11111112222");
 
     }
 }
